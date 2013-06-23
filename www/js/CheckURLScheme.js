e94bd3a0f2815c2524fbfcb827046cb542cee430
ohh2ahh <luca.schneider@me.com>
2013-06-23 13:24:06 +0300

    Create CheckURLScheme.js

---
+++ www/js/CheckURLScheme.js
@@ -0,0 +1,9 @@
+window.checkurlscheme = function(str, callback) {
+    cordova.exec(
+        callback,                           // successFunction
+        function(err) { callback(false); }, // failFunction
+        "CheckURLScheme",                   // service
+        "checkurlscheme",                   // action
+        [str]                               // [<args>]
+    );
+};
