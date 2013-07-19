380c6b8626b54106aacbd259fca85c258f01ee99
ohh2ahh <luca.schneider@me.com>
2013-07-19 22:17:36 +0200

    Update and rename CheckURLScheme.m to SchemeCheck.m

---
+++ src/ios/SchemeCheck.m
@@ -0,0 +1,21 @@
+#import "SchemeCheck.h"
+#import <Cordova/CDV.h>
+
+@implementation SchemeCheck
+
+- (void)schemecheck:(CDVInvokedUrlCommand*)command
+{
+	CDVPluginResult* pluginResult = nil;
+	NSString* scheme = [command.arguments objectAtIndex:0];
+	
+	if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:scheme]]) {
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:(true)];
+	}
+	else {
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:(false)];
+	}
+	
+	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
