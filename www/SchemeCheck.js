e667f418c5180de9fe9e56ef68c68ebe9f8574c5
ohh2ahh <luca.schneider@me.com>
2013-07-19 22:16:30 +0200

    Update SchemeCheck.js

--- www/SchemeCheck.js
+++ www/SchemeCheck.js
@@ -1,9 +1,9 @@
 window.schemecheck = function(str, callback) {
 	cordova.exec(
-		callback,			// success
-		callback,			// error
-		"SchemeCheck",		// service
-		"schemecheck",		// action
-		[str]				// argument
+		callback,
+		callback,
+		"SchemeCheck",
+		"schemecheck",
+		[str]
 	);
 };
