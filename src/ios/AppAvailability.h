ca826513bd98f9b6a2d2f6e11164f92b4b3e5108
ohh2ahh <luca.schneider@me.com>
2013-07-20 02:33:18 +0200

    Update and rename SchemeCheck.h to AppAvailability.h

---
+++ src/ios/AppAvailability.h
@@ -0,0 +1,7 @@
+#import <Cordova/CDV.h>
+
+@interface AppAvailability : CDVPlugin
+
+- (void)appavailability:(CDVInvokedUrlCommand*)command;
+
+@end
--- src/ios/SchemeCheck.h
+++
@@ -1,7 +0,0 @@
-#import <Cordova/CDV.h>
-
-@interface SchemeCheck : CDVPlugin
-
-- (void)schemecheck:(CDVInvokedUrlCommand*)command;
-
-@end
