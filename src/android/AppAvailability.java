c5814c1cdcba01571ee979d5962c75e5bb2e5299
ohh2ahh <luca.schneider@me.com>
2013-07-20 02:34:18 +0200

    Create AppAvailability.java

---
+++ src/android/AppAvailability.java
@@ -0,0 +1,45 @@
+package com.ohh2ahh.appavailability;
+
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+public class AppAvailability extends CordovaPlugin {
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		if (action.equals("appavailability")) {
+			String uri = args.getString(0);
+			this.appavailability(uri, callbackContext);
+			return true;
+		}
+		return false;
+	}
+	
+	// Thanks to http://floresosvaldo.com/android-cordova-plugin-checking-if-an-app-exists
+	public boolean appInstalled(String uri) {
+		Context ctx = this.cordova.getActivity().getApplicationContext();
+    	final PackageManager pm = ctx.getPackageManager();
+    	boolean app_installed = false;
+    	try {
+    		pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+    		app_installed = true;
+    	}
+    	catch(PackageManager.NameNotFoundException e) {
+    		app_installed = false;
+    	}
+    	return app_installed;
+    }
+	
+	private void appavailability(String uri, CallbackContext callbackContext) {
+		if(appInstalled(uri)) {
+			callbackContext.success();
+		}
+		else {
+			callbackContext.error("App not available");
+		}
+	}
+}
